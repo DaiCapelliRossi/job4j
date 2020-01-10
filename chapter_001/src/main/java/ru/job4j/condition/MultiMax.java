0c53e033c5f6cf13617dd51c27ee10a5c560f826
DaiCapelliRossi <ness.ostroumova@gmail.com>
2020-01-11 00:33:14 +0300

    Интерграция Git и IDEA [#159462]

---
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class MultiMax {
+    public int max(int first, int second, int third) {
+        int result;
+        boolean maximum = first > second;
+        if (maximum) {
+            maximum = first > third;
+            result = maximum ? first : third;
+        } else {
+            maximum = second > third;
+            result = maximum? second : third;
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.condition;
+
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SqMaxTest {
+    @Test
+    public void firstIsMax() {
+        int result = SqMax.max(4,3,2,1);
+        assertThat(result, Is.is(4));
+    }
+
+    @Test
+    public void secondIsMax() {
+        int result = SqMax.max(3,4,2,1);
+        assertThat(result, Is.is(4));
+    }
+
+    @Test
+    public void thirdIsMax() {
+        int result = SqMax.max(2,3,4,1);
+        assertThat(result, Is.is(4));
+    }
+
+    @Test
+    public void forthtIsMax() {
+        int result = SqMax.max(1,3,2,4);
+        assertThat(result, Is.is(4));
+    }
+}
