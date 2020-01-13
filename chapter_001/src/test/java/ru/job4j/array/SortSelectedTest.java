95ba42d427aa866d2192effc4cedccaec445d6d2
DaiCapelliRossi <ness.ostroumova@gmail.com>
2020-01-13 23:21:46 +0300

    Интерграция Git и IDEA [#159462]

--- chapter_001/src/test/java/ru/job4j/array/SortSelectedTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -6,10 +6,18 @@ import static org.junit.Assert.assertThat;
 
 public class SortSelectedTest {
     @Test
-    public void whenSort() {
+    public void whenSortFive() {
         int[] input = new int[] {3, 4, 1, 2, 5};
         int[] result = SortSelected.sort(input);
         int[] expect = new int[] {1, 2, 3, 4, 5};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSortThree() {
+        int[] input = new int[] {9, 7, 8};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {7, 8, 9};
         assertThat(result, is(expect));
     }
 }
