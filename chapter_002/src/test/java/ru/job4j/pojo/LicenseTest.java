db567f8d5d92c41ce71b757467c653e2fb82cbbf
DaiCapelliRossi <ness.ostroumova@gmail.com>
2020-04-07 17:38:53 +0300

    4. Сравнение моделей. Метод equals. [00033]

---
+++ chapter_002/src/test/java/ru/job4j/pojo/LicenseTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.pojo;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+
+public class LicenseTest {
+
+    @Test
+    public void eqName() {
+        License first = new License();
+        first.setCode("audio");
+        first.setOwner("Ness");
+        License second = new License();
+        second.setCode("audio");
+        second.setOwner("Ness");
+        assertThat(first, is(second));
+    }
+}
