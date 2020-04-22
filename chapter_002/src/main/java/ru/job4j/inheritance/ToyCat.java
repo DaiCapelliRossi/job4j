9303c39eae054787a495674569f4e246fdb2a024
DaiCapelliRossi <ness.ostroumova@gmail.com>
2020-04-22 22:05:50 +0300

    6. Manifest для проекта Tracker [00044]

--- chapter_002/src/main/java/ru/job4j/inheritance/ToyCat.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/ToyCat.java
@@ -4,6 +4,7 @@ public class ToyCat extends Cat {
     public boolean canBeWashByWashMachine() {
         return true;
     }
+
     public static void main(String[] args) {
         Cat cat = new Cat();
         System.out.println(cat.canPurr());
