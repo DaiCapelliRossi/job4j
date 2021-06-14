39af50c372af924e74684fbd265659743667cb80
DaiCapelliRossi <ness.ostroumova@gmail.com>
2021-06-14 22:56:39 +0300

    3.4.0. Сортировка [00078]

--- chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -2,9 +2,52 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
+import java.util.*;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 public class TrackerTest {
+
+    @Test
+    public void sortByID() {
+        List<Item> items = new ArrayList<>();
+        Item item1 = new Item("c");
+        Item item2 = new Item("a");
+        Item item3 = new Item("b");
+        items.add(item1);
+        items.add(item2);
+        items.add(item3);
+
+        Collections.sort(items, new ItemCompareAscending());
+        String[] sortedItems = new String[3];
+        for (int i = 0; i < 3; i++) {
+            sortedItems[i] = items.get(i).getName();
+        }
+
+        assertThat(sortedItems, is(new String[]{"a", "b", "c"}));
+    }
+
+    @Test
+    public void sortByIDReverse() {
+        List<Item> items = new ArrayList<>();
+        Item item1 = new Item("c");
+        Item item2 = new Item("a");
+        Item item3 = new Item("b");
+        items.add(item1);
+        items.add(item2);
+        items.add(item3);
+
+        Collections.sort(items, new ItemCompareDescending());
+
+        String[] sortedItems = new String[3];
+        for (int i = 0; i < 3; i++) {
+            sortedItems[i] = items.get(i).getName();
+        }
+
+        assertThat(sortedItems, is(new String[]{"c", "b", "a"}));
+    }
+
+
 /*
     @Test
     public void whenAddNewItemThenTrackerHasSameItem() {
