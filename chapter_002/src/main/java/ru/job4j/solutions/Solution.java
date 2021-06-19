592b5d7aefd9f68e1d362f0c19b69611f29b607c
DaiCapelliRossi <ness.ostroumova@gmail.com>
2021-06-19 20:48:14 +0300

    3.5.2. Travis CI [00085]

--- chapter_002/src/main/java/ru/job4j/solutions/Solution.java
+++ chapter_002/src/main/java/ru/job4j/solutions/Solution.java
@@ -1,4 +1,16 @@
 package ru.job4j.solutions;
 
 public class Solution {
+    public static void main(String[] args) {
+        Pegasus horse = new Pegasus();
+    }
+    public static interface CanFly {
+        public void fly();
+    }
+    public static class Horse {
+        public void run() {
+        }
+    }
+    public static class Pegasus {
+    }
 }
