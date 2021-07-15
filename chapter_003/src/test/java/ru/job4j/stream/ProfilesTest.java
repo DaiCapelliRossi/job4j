909344d4c3aeed470114eb25b21e12ee26154dd6
DaiCapelliRossi <ness.ostroumova@gmail.com>
2021-07-15 19:44:35 +0300

    5.2. Список адресов [00102]

---
+++ chapter_003/src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+import ru.job4j.search.Person;
+import ru.job4j.search.PhoneDictionary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+    @Test
+    public void whenCollect() {
+        List<Profile> profile = new ArrayList<>();
+        profile.add(new Profile(new Address("Moscow", "Arbat str.", 17, 29)));
+        profile.add(new Profile(new Address("St.Petersburg", "Nevskiy pr.", 27, 129)));
+        Profiles profiles = new Profiles();
+        List<Address> addresses = profiles.collect(profile);
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("Moscow", "Arbat str.", 17, 29));
+        expected.add(new Address("St.Petersburg", "Nevskiy pr.", 27, 129));
+
+        assertThat(addresses, is(expected));
+    }
+}
